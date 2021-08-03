da4add3b795819f758be157ed5769e7bde1a6730
Stuart B. Wilkins <swilkins@bnl.gov>
2021-08-03 05:38:54 -0400

    Added flags for probe type

--- mysql/create_database.sql
+++ mysql/create_database.sql
@@ -3,13 +3,18 @@ CREATE DATABASE arptools;
 USE arptools;
 
 CREATE TABLE arpdata(
-  hw_address CHAR(17) NOT NULL,
-  ip_address CHAR(15),
-  hostname   VARCHAR(256),
-  location   VARCHAR(256) NOT NULL,
-  label      VARCHAR(256) NOT NULL,
-  last_seen  DATETIME,
-  created    DATETIME DEFAULT CURRENT_TIMESTAMP,
-  registered BOOL DEFAULT false NOT NULL,
+  hw_address    CHAR(17) NOT NULL,
+  ip_address    CHAR(15),
+  hostname      VARCHAR(256),
+  location      VARCHAR(256) NOT NULL,
+  label         VARCHAR(256) NOT NULL,
+  last_seen     DATETIME,
+  created       DATETIME DEFAULT CURRENT_TIMESTAMP,
+  registered    BOOL DEFAULT false,
+  type_arp      BOOL DEFAULT false,
+  type_udp      BOOL DEFAULT false,
+  type_dhcp     BOOL DEFAULT false,
+  dhcp_name     VARCHAR(256),
+  visible       BOOL DEFAULT true,
   PRIMARY KEY (hw_address)
 );
