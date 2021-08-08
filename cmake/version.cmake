47c3033ae06ed8c29f45af5dbf525b243a4124eb
Stuart B. Wilkins <swilkins@bnl.gov>
2021-08-08 12:49:11 -0400

    Cleanup indenting

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -5,36 +5,41 @@ execute_process(COMMAND bash -c "git log --pretty=format:'%h' -n 1"
 # always the case, e.g. when someone downloaded a zip
 # file from Github instead of a checkout)
 if ("${GIT_REV}" STREQUAL "")
-    set(GIT_DIFF "")
-	set(GIT_VERSION "UNKNOWN")
-    set(GIT_BRANCH "N/A")
+  set(GIT_DIFF "")
+  set(GIT_VERSION "UNKNOWN")
+  set(GIT_BRANCH "N/A")
 else()
-    execute_process(
-        COMMAND bash -c "git diff --quiet --exit-code || echo +"
-        OUTPUT_VARIABLE GIT_DIFF)
-    execute_process(
-		COMMAND bash -c "git describe --always --tags || echo UNKNOWN"
-		OUTPUT_VARIABLE GIT_VERSION ERROR_QUIET)
-    execute_process(
-        COMMAND git rev-parse --abbrev-ref HEAD
-        OUTPUT_VARIABLE GIT_BRANCH)
+  execute_process(
+    COMMAND bash -c "git diff --quiet --exit-code || echo +"
+    OUTPUT_VARIABLE GIT_DIFF)
+  execute_process(
+    COMMAND bash -c "git describe --always --tags || echo UNKNOWN"
+    OUTPUT_VARIABLE GIT_VERSION ERROR_QUIET)
+  execute_process(
+    COMMAND git rev-parse --abbrev-ref HEAD
+    OUTPUT_VARIABLE GIT_BRANCH)
 
-	string(STRIP "${GIT_VERSION}" GIT_VERSION)
-	string(STRIP "${GIT_REV}" GIT_REV)
-    string(STRIP "${GIT_DIFF}" GIT_DIFF)
-    string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
+  string(STRIP "${GIT_VERSION}" GIT_VERSION)
+  string(STRIP "${GIT_REV}" GIT_REV)
+  string(STRIP "${GIT_DIFF}" GIT_DIFF)
+  string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
+
 endif()
+
+string(REPLACE "v" "" VERSION_NUMBER "${GIT_VERSION}")
+set(VERSION_NUMBER "${VERSION_NUMBER}" CACHE INTERNAL "GIT version")
+message(STATUS "CACHE VERSION_NUMBER = ${VERSION_NUMBER}")
 
 set(VERSION "const char* ARPTOOLS_GIT_REV=\"${GIT_REV}\";
 const char* ARPTOOLS_GIT_BRANCH=\"${GIT_BRANCH}\";
 const char* ARPTOOLS_GIT_VERSION=\"${GIT_VERSION}${GIT_DIFF}\";")
 
 if(EXISTS ${CMAKE_BINARY_DIR}/version.c)
-	file(READ ${CMAKE_BINARY_DIR}/version.c VERSION_)
+  file(READ ${CMAKE_BINARY_DIR}/version.c VERSION_)
 else()
-    set(VERSION_ "")
+  set(VERSION_ "")
 endif()
 
 if (NOT "${VERSION}" STREQUAL "${VERSION_}")
-	file(WRITE ${CMAKE_BINARY_DIR}/version.c "${VERSION}")
+  file(WRITE ${CMAKE_BINARY_DIR}/version.c "${VERSION}")
 endif()
