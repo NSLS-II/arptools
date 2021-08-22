eb6756a7692e120300baa8dc5eef093319d056eb
Stuart B. Wilkins <swilkins@bnl.gov>
2021-08-22 12:00:24 -0400

    Change Key Fields

--- mysql/create_database.sql
+++ mysql/create_database.sql
@@ -4,11 +4,11 @@ USE arptools;
 
 CREATE TABLE arpdata(
   hw_address        CHAR(17) NOT NULL,
+  vlan              SMALLINT NOT NULL,
+  location          VARCHAR(256) NOT NULL,
+  label             VARCHAR(256),
   ip_address        CHAR(15),
   hostname          VARCHAR(256),
-  vlan              SMALLINT,
-  location          VARCHAR(256) NOT NULL,
-  label             VARCHAR(256) NOT NULL,
   type              SMALLINT UNSIGNED DEFAULT 0,
   last_seen         DATETIME,
   created           DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -17,5 +17,5 @@ CREATE TABLE arpdata(
   last_audited      DATETIME,
   dhcp_name         VARCHAR(256),
   visible           BOOL DEFAULT TRUE,
-  PRIMARY KEY (hw_address)
+  PRIMARY KEY (hw_address, vlan, location)
 );
