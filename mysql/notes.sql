f0a5186c2928a1e9b6407ea684d1fdab1915bb57
Stuart B. Wilkins <swilkins@bnl.gov>
2021-09-26 10:44:49 -0400

    Added sql check for daemon running

--- mysql/notes.sql
+++ mysql/notes.sql
@@ -2,3 +2,7 @@
 
 INSERT INTO arpdata_old SELECT * FROM arpdata WHERE DATE(last_seen) < DATE_SUB(CURDATE(), INTERVAL 5 DAY);
 DELETE FROM arpdata WHERE DATE(last_seen) < DATE_SUB(CURDATE(), INTERVAL 5 DAY);
+
+# Check on daemon for any hosts offline for 1 hr
+
+SELECT DISTINCT hostname FROM daemondata WHERE last_updated < DATE_SUB(NOW(), INTERVAL 1 HOUR);
